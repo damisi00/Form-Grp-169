b655253d1450e52fd154779ccad8af7193df87e4
[damisi00] <enioluwafeo@gmail.com>
2022-05-15 02:37:13 +0100

    completed the styling of the form

--- js/script.js
+++ js/script.js
@@ -13,3 +13,33 @@ function show(){
 function close(){
     mainMenu.style.top = "-100%"
 }
+
+
+// For the password visibility
+const togglePassword = document.querySelector("#togglePwd");
+const toggleConfirmPassword = document.querySelector("#toggleConfirmPwd");
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirmPwd");
+
+togglePassword.addEventListener("click", function () {
+    // toggle type attribute
+    const type = password.getAttribute("type") === "password" ? "text" : "password";
+    password.setAttribute("type", type);
+
+    // to toggle eye icon
+    this.classList.toggle("bi-eye");
+});
+toggleConfirmPassword.addEventListener("click", function () {
+    // toggle type attribute
+    const type = confirmPassword.getAttribute("type") === "password" ? "text" : "password";
+    confirmPassword.setAttribute("type", type);
+
+    // to toggle eye icon
+    this.classList.toggle("bi-eye");
+});
+
+// prevent form submit
+const form = document.getElementsByTagName("form");
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+});
